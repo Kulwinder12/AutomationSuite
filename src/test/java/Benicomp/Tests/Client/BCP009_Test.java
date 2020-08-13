5e61f6b56549ae30efa2504a58fbb820557ae6a1
kulwinder <kulwinder.singh@smartdatainc.net>
2020-08-13 17:07:14 +0530

    BCP009_Test and BCP008_Test

---
+++ src/test/java/Benicomp/Tests/Client/BCP009_Test.java
@@ -0,0 +1,30 @@
+package Benicomp.Tests.Client;
+
+import Benicomp.Modules.Client;
+import Benicomp.Modules.LoginOut;
+import Benicomp.TestData.GlobalTestData;
+import Benicomp.Utils.TestBase;
+import Benicomp.Utils.WaitTool;
+import org.testng.annotations.Test;
+
+public class BCP009_Test extends TestBase {
+
+    LoginOut objLoginOut = new LoginOut();
+
+    WaitTool wt = new WaitTool();
+
+    @Test(description = "To verify that Primary contact is required to send credentials.", groups = {"Client"})
+    public void BCP009() throws Exception {
+
+        GlobalTestData.Client_Users.GetData(getClass().getSimpleName());
+        //  GlobalTestData.Contact_Users.GetData(getClass().getSimpleName());
+        logTestStep("Log in to application");
+        objLoginOut.loginAs(LoginOut.Actor.USERNAME_USERS);
+
+        wt.implicitwait();
+        Client objClient = new Client();
+        objClient.verifyPrimaryContactForsendCredentials();
+
+
+    }
+}
